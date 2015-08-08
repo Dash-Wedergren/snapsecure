7214fcdfe845f66cf6b4b0594065788fce3444dc
Dash-Wedergren <dashwedergren@gmail.com>
2015-08-08 16:31:33 -0500

    added encrypt/decrypt functions and improved sanatizing function

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ var fs = require('fs');
 var crypto = require('crypto');
 var express = require('express');
 var tls = require('tls');
+var cluster = require('cluster');
 var bodyParser = require('body-parser');
 var app = express();
 var argv = require('yargs')
@@ -18,7 +19,7 @@ res.header('Access-Control-Allow-Origin', '*');
 res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
 res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With');
 next();
-})
+});
 app.use(function(req, res, next) {
 console.log('Connection made from: ', req.ip, ' to ', req.path, ' with ', req.protocol);
 next();
@@ -28,46 +29,67 @@ app.get('/',function(req, res){
 res.send("hello");
 });
 
+var db;
+
 /**{string}
  *@param {string} unsafe 
  */
 function sanaitize(unsafe){
 var safe;
 if(typeof unsafe === "string"){
-safe = unsafe.replace(/[^a-z0-9]/gim,"");
+safe = unsafe.replace(/[^a-z0-9-_]/gim,"");
 return safe;
 }else{
-console.log("Not string");
+console.log(unsafe + " is not a string");
 return "undefined"
-}
+}};
+
+/**
+ * @param {string} path argv.f
+ * @param {string} key argv.s
+ */
+function decrypt(){
+db = fs.readFileSync(argv.f).toString();
+var input = crypto.createDecipher('AES-256-CBC',argv.s);
+input.update(db, 'utf8');
+db = input.final('hex');
 }
 
-/** {Object}
+/*
+ * @param {string} path argv.f
+ * @param {string} key argv.s
+ */
+function encrypt(){
+var output = crypto.createCipher('AES-256-CBC',argv.s);
+output.update(db, 'utf8');
+db = ouput.final('hex'); 
+fs.writeFileSync(argv.f,db);
+}
+
+/** {JSON encoded Object}
  * @param {string} username
  * @param {(PEM encoded) string} public_key
  */
 app.post('/newuser', function(req, res){
-var db = fs.readFileSync(argv.f).toString();
 var user_data = req.body;
 console.log(user_data);
 res.sendStatus(200);
 });
 
-/** {Object}
+/** {JSON encoded Object}
  * @param {string} username
  */
 app.post('/key_lookup', function(req, res){
-var db = fs.readFileSync(argv.f).toString();
+//decrypt();
+db = fs.readFileSync(argv.f).toString();
 db = JSON.parse(db);
-console.log(typeof db);
 var user_request = sanaitize(req.body.user);
-console.log(user_request);
-eval("var user_key = db." + user_request + ".pub_key");
-if(!user_key){
-res.send("user not found");
-return;
+var user_key;
+try{user_key = db[user_request].pub_key}
+catch(err){
+res.send("user not found")
 }
 res.send(user_key);
-})
+});
 
 app.listen(8080);
